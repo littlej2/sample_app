bac1aaf17b10826f5e7c93a7abb44363ac7d9869
Janelle Littlejohn <=>
2012-04-06 05:00:46 -0400

    Chapter 8 Exercises complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -90,11 +90,32 @@ describe UsersController do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end    
-    end
-     it "should have a welcome message" do
-        post :create, :user => @attr
-        flash[:success].should =~ /welcome to the sample app/i
-      end
-    end
+
+  it "should have a welcome message" do
+    post :create, :user => @attr
+    flash[:success].should =~ /welcome to the sample app/i
   end
+  end
+    end
+
+   it "should have a name field" do
+     get :new
+     response.should have_selector("input[name='user[name]'][type='text']")
+   end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+     get :new
+    response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+
 end
